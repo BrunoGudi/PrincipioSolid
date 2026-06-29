265acf0c860609501852bbf6acec0cbfe2782966
Bruno Gudiño <brunogudi06@gmail.com>
2026-06-29 13:53:19 -0300

    ultimo commit

--- src/main/java/com/mycompany/solidplataformaeduvirtual/Evaluaciones/Evaluacion.java
+++ src/main/java/com/mycompany/solidplataformaeduvirtual/Evaluaciones/Evaluacion.java
@@ -1,9 +1,9 @@
 package com.mycompany.solidplataformaeduvirtual.Evaluaciones;
 /**
- * PATRÓN STRATEGY (Estrategia
+ * PATRÓN STRATEGY
  * Esta interfaz define el contrato para la "estrategia" de cálculo de nota.
  * Permite cambiar dinámicamente el algoritmo utilizado para calcular la nota final
- * (examen, trabajo práctico o proyecto final) sin modificar el código que lo consume (cumpliendo también el principio OCP de SOLID).
+ * (examen, trabajo práctico o proyecto final) sin modificar el código que lo consume 
  */
 public interface Evaluacion {
     // Calcula la nota final segun el tipo de evaluacion
